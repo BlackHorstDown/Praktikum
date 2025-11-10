e990f15c02845a26474281fd6687c32c69be048d
nilsnoll <nils.noll@googlemail.com>
2025-11-10 03:01:24 +0100

    schrott

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    int a = 2;
-    int b = 3;
-    int c = 8;
+public class Main {
 
-    float result = ((float)a+b+c)/3;
-    // Minimum
-    System.out.println(result+ " Mittelwert") ;
-    int minimum = a;
-    if (a>b){
-        minimum = b;
-    }
-    if (minimum>c){
-        minimum = c;
-    }
-    System.out.println(minimum+ " Minimum") ;
+    public static void main(String[] args) { // <--- Korrigierte Signatur der main-Methode
+        int a = 2;
+        int b = 3;
+        int c = 8;
 
-    int n = 4;
-    int quadrat = 0;
-    int summe = 0;
-    for (int i=1; i<=n; i++){
-        quadrat = i*i;
-        summe = summe+quadrat;
-    }
-    System.out.println(summe+ " Quadrat") ;
-
-
-    int t = 2;
-    int ergebnis = 0;
-    if (t < 0) {
-        ergebnis = 10;
-    }
-    else if (t < 5 && 0<=t){
-        ergebnis = 10 - 2*t;
-    }
-    else {
-        ergebnis = 0;
-    }
-
-    System.out.println(ergebnis+ " ergebnis") ;
-
-    muster(5);
-}
-public static void muster(int n) {
-    int muster = 0;
-    for (int i=1; i<=n; i++){
-        muster = (muster*10) +i;
-        String punkte = "";
-        for (int j=1; j<=n-i; j++){
-            punkte = punkte + ".";
-
+        float result = ((float)a+b+c)/3;
+        // Minimum
+        System.out.println(result + " Mittelwert") ;
+        int minimum = a;
+        if (a>b){
+            minimum = b;
         }
-        System.out.println(muster + punkte);
+        if (minimum>c){
+            minimum = c;
+        }
+        System.out.println(minimum + " Minimum") ;
+
+        int n = 4;
+        int quadrat = 0;
+        int summe = 0;
+        for (int i=1; i<=n; i++){
+            quadrat = i*i;
+            summe = summe+quadrat;
+        }
+        System.out.println(summe + " Quadrat") ;
+
+
+        int t = 2;
+        int ergebnis = 0;
+        if (t < 0) {
+            ergebnis = 10;
+        }
+        else if (t < 5 && 0<=t){
+            ergebnis = 10 - 2*t;
+        }
+        else {
+            ergebnis = 0;
+        }
+
+        System.out.println(ergebnis + " ergebnis") ;
+
+        muster(5);
+    } 
+
+    // Die muster-Methode muss auch IN die Klasse!
+    public static void muster(int n) {
+        int muster = 0;
+        for (int i=1; i<=n; i++){
+            muster = (muster*10) +i;
+            String punkte = "";
+            for (int j=1; j<=n-i; j++){
+                punkte = punkte + ".";
+
+            }
+            System.out.println(muster + punkte);
+        }
     }
+
 }
